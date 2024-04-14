e27d344316ab754fe8d8d032878a75a8ff777fe6
Daneris Mendoza <106788394+DanerisMendoza@users.noreply.github.com>
2024-04-14 22:10:27 +0800

    user : data-table init

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { RootState } from "../store/store";
 import { useSelector, useDispatch } from 'react-redux'
-import { set_register_dialog, set_login_dialog } from '../features/user/components'
-import { set_user_details, clear_user_details, getUserDetails, getUsers } from '../features/user/details'
+import { getUserDetails, getUsers } from '../features/user/details'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useState, useEffect } from 'react'
+
+const columns: GridColDef[] = [
+    { field: 'name', headerName: 'name', flex: 1, align: 'center', headerAlign: 'center', },
+    { field: 'username', headerName: 'username', flex: 1, align: 'center', headerAlign: 'center', },
+    { field: 'role', headerName: 'role', flex: 1, align: 'center', headerAlign: 'center', },
+];
 
 export default function Users() {
     const dispatch = useDispatch()
@@ -15,10 +21,10 @@ export default function Users() {
     );
     useEffect(() => {
         getUserDetails()(dispatch)
-    }, []); 
+    }, []);
     useEffect(() => {
         getUsers()(dispatch)
-    }, []); 
+    }, []);
 
     useEffect(() => {
         // console.log(userDetails)
@@ -28,7 +34,22 @@ export default function Users() {
         console.log(users)
     }, [users]);
 
-    return <div className="flex flex-col  h-full w-full  p-8 mt-4">
-        <p>Users Page</p>
+    return <div className="flex flex-col  h-full w-screen  p-8 mt-4">
+        <div style={{ width: '90%', height: '90%', margin: 'auto' }}>
+            <div>
+                <DataGrid
+                    rows={users}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                    getRowId={(row) => row._id}
+                />
+            </div>
+        </div>
     </div>;
 }
