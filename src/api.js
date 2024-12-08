eb9abed6c4fd6c41037e2c1723d57adf63f4d5d9
Daneris Mendoza <danerismendoza096@gmail.com>
2024-12-08 21:23:14 +0800

    Update Config - Add Dev and Prod Build - Add Readme - Use env Enpoint Depends on build

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'http://localhost:9000/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
